579565fbfcb12887064ec7fbb9a7a68508257102
Darren Ipad <djdunford@gmail.com>
2019-02-03 10:04:36 +0000

    wip

--- taximeter.py
+++ taximeter.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 import fourletterphat
+import time
+import RPi.GPIO as GPIO
 
 while True:
+
+    # show for hire message
     fourletterphat.clear()
-    fourletterphat.scroll_print("UBER DAVE FOR HIRE", 0.2)
+    fourletterphat.scroll_print("UBER DAVE  -  FOR HIRE", 0.2)
+    # button 2 held down should initiate a shutdown
+    
+    # button 1 starts the hiring sequence
+    
