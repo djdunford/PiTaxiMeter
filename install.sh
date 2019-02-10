0547e8681cb987c07ab0485f7ec00c3cd35321bc
djdun <djdun@BIRTSMORTON.Home>
2019-02-10 20:49:44 +0000

    updated install.sh and added service file

--- install.sh
+++ install.sh
@@ -2,32 +2,32 @@
 #
 # install.sh
 #
-# install the service
+# install PiTaxiMeter as a service under systemd control
 # 
 # by Darren Dunford
 #
 
 # script control variables
-appdir="/opt/xmastrain"
+appdir="/opt/pitaximeter"
 
 # update package manager
 apt-get update
 
 # install required modules
-apt-get install -y apache2 libapache2-mod-wsgi-py3 python3-pip
-pip3 install bottle AWSIoTPythonSDK gpiozero
+apt-get install -y python3 python3-pip
+pip3 install gpiozero
 
 # create directory structure for app and webapp
 mkdir -p $appdir
 chgrp -R pi $appdir
 
 # copy webapp and wsgi files
-cp xmastrain.py $appdir
+cp pitaximeter.py $appdir
 
 # install service file
-cp xmastrain.service /lib/systemd/system
-chmod 644 /lib/systemd/system/xmastrain.service
+cp pitaximeter.service /lib/systemd/system
+chmod 644 /lib/systemd/system/pitaximeter.service
 
 # restart Apache and xmastrain service
 systemctl daemon-reload
-systemctl restart xmastrain
+systemctl restart pitaximeter
